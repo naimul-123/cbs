430eb079614e9cc38cfbcdbf2f700ca8714b15b0
Naimul Islam <101422148+naimul-123@users.noreply.github.com>
2024-09-04 05:20:51 +0600

    Implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,28 @@
+import MobileNab from "@/components/MobileNab";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const loggedIn = { firstName: 'Naimul', lastName: 'Islam' }
     return (
-        <main>
-            SIDEBAR
-            {children}
+        <main className="flex h-screen w-full font-inter">
+            <Sidebar user={loggedIn} />
+            <div className="flex size-full flex-col">
+
+                <div className="root-layout">
+                    <Image src="/icons/logo.svg" alt="menu-icon" width={30} height={30} />
+                    <div>
+
+                        <MobileNab user={loggedIn} />
+                    </div>
+                </div>
+                {children}
+            </div>
+
         </main>
     );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-    const loggedIn = { firstName: 'Naimul' }
+    const loggedIn = { firstName: 'Naimul', lastName: 'Islam', email: 'contact@gmail.com' }
     return (
         <section className='home'>
             <div className='home-content'>
@@ -20,6 +21,7 @@ const Home = () => {
 
                 />
             </div>
+            <RightSidebar user={loggedIn} transections={[]} banks={[{ currentBalance: 123.50 }, {}]} />
         </section>
     )
 }
